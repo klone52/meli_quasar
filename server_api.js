bf25428185b709dd146dca0d66e698a651c1648c
Nicolas Hernandez <nhernandezpena@gmail.com>
2022-01-02 23:56:23 -0300

    Deleted offset of messages

--- server_api.js
+++ server_api.js
@@ -8,16 +8,47 @@ function GetLocation(distances ) {
 }
 
 function GetMessage(messages) { 
-    let emisor_message = ""; 
+    let emisor_message = "";
+    var min_length = 100000;
     
-    console.log(arguments.length);
-
+    // Check correct type of input or throws error
     if (arguments.length === 0) throw new Error("No argument passed!");
     if (!Array.isArray(messages)) throw new Error("Input not an array!"); 
+    
+    // If no messages return empty message
     if (messages.length === 0) return emisor_message ; 
+
+    //Get min length of messages
+    messages.forEach(satelite_msg => {
+        if (satelite_msg.length <= min_length) min_length = satelite_msg.length;
+    });
+    console.log('min length',min_length);
+
+    // Delete offset
+    messages.forEach(satelite_msg => {
+        while (satelite_msg.length > min_length) {
+            satelite_msg.shift();
+        };
+    });
+
+    // Restore message
+    messages.forEach(satelite_msg => {
+        while (satelite_msg.length > min_length) {
+            satelite_msg.shift();
+        };
+        console.log(satelite_msg);
+    });
+
+
 
     return emisor_message;
 }
+
+var kenobi_msg = ["", "este", "es", "un", "mensaje"]; // 5
+var skywalker_msg = ["este", "", "un", "mensaje"];  // 4
+var sato_msg = ["", "", "es", "", "mensaje"];  // 5
+
+GetMessage([kenobi_msg, skywalker_msg, sato_msg]);
 
 module.exports = GetLocation;
 module.exports = GetMessage;
